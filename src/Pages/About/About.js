703b0536637bd723674bdb12ebccd9c93c22b984
Forhad <farhadhossain01992@gmail.com>
2022-12-30 19:37:29 +0600

    media card displayy

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,6 +6,8 @@ const About = () => {
             <h2 className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore repellat, modi alias maxime ea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo mea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo magni rfherbfebr  rhfeiorf nerg reprehenderit dicta!</h2>
             
             <h2 className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore repellat, modi alias maxime ea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo mea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo magni rfherbfebr  rhfeiorf nerg reprehenderit dicta!</h2>
+            
+            <h2 className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore repellat, modi alias maxime ea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo mea eveniet pariatur qui est error! Iure aliquam quae vitae explicabo sint qui nemo magni rfherbfebr  rhfeiorf nerg reprehenderit dicta!</h2>
         </div>
     );
 };
